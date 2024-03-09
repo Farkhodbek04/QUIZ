956f72d95aac3a7bde07836e9cc76f8ca36380d4
Farxodbek <farhodbekjaloldinov@gmail.com>
2024-03-09 08:36:21 +0500

    CKeditor-5 has been added!

--- Quiz/settings.py
+++ Quiz/settings.py
@@ -125,9 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static/']
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-STATIC_ROOT = BASE_DIR / 'static'
+# STATIC_ROOT = BASE_DIR / 'static'
 
 CKEDITOR_5_FILE_STORAGE = "main.storage.CustomStorage"
 
